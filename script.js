0038e19486009bb5e26d72b3e818be6dacf9f7ef
NHWai <nayhtetwinee@gmail.com>
2023-03-18 20:34:18 +0630

    apu url changed to on render url

--- script.js
+++ script.js
@@ -13,8 +13,8 @@ const uploadImgForm = document.querySelector(".uploadImgForm");
 const uploadBtn = document.querySelector(".uploadBtn");
 const fileEl = document.querySelector("#file");
 
-// const api = "https://node-crude-app.onrender.com";
-const api = "http://localhost:3000/";
+const api = "https://node-crude-app.onrender.com";
+// const api = "http://localhost:3000/";
 // const api = window.location.href;
 
 let closeElArr;
